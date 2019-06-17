61fd810ee5843be5ba72e1b8fc6fa687039c5928
Daryl Dalan <29158851+ddarylmae@users.noreply.github.com>
2019-06-18 11:04:03 +1200

    Extracted methods for getting employee name and period

--- PayslipProblemVer2/PayslipGenerator.cs
+++ PayslipProblemVer2/PayslipGenerator.cs
@@ -1,11 +1,28 @@
+using System;
+
 namespace PayslipProblemVer2
 {
     public class PayslipGenerator
     {
         public string GetPayslip(Employee employee, Period period)
         {
-            return $"Name: {employee.Name} {employee.Surname} \n" +
-                   $"Pay Period: {period.StartDate.ToString("dd MMMM")} - {period.EndDate.ToString("dd MMMM")}";
+            return $"{GetName(employee)}\n" +
+                   GetPeriod(period);
+        }
+
+        private string GetPeriod(Period period)
+        {
+            return $"Pay Period: {GetFormattedDate(period.StartDate)} - {GetFormattedDate(period.EndDate)}";
+        }
+
+        private string GetFormattedDate(DateTime date)
+        {
+            return date.ToString("dd MMMM");
+        }
+
+        private string GetName(Employee employee)
+        {
+            return $"Name: {employee.Name} {employee.Surname}";
         }
     }
 }
