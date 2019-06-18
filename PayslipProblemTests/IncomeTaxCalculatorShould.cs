bd86cc9833e28619f82def7631391cc293441dae
Daryl Dalan <29158851+ddarylmae@users.noreply.github.com>
2019-06-18 21:02:08 +1200

    Added last range for pay range list

--- PayslipProblemTests/IncomeTaxCalculatorShould.cs
+++ PayslipProblemTests/IncomeTaxCalculatorShould.cs
@@ -16,6 +16,9 @@ namespace PayslipProblemTests
         [InlineData(87000, 19822)]
         [InlineData(87001, 19822)]
         [InlineData(100000, 24632)]
+        [InlineData(180000, 54232)]
+        [InlineData(180001, 54232)]
+        [InlineData(200000, 63232)]
         public void Return_correct_incomeTax(uint annualSalary, uint expectedIncomeTax)
         {
             var incomeTaxCalculator = new IncomeTaxCalculator();
--- PayslipProblemTests/PayRange.cs
+++ PayslipProblemTests/PayRange.cs
@@ -11,6 +11,7 @@ namespace PayslipProblemTests
 
         private static readonly List<PayRange> PayRanges = new List<PayRange>
         {
+            new PayRange {LowerLimit = 180001, Amount = 54232, Excess = 0.45},
             new PayRange {LowerLimit = 87001, Amount = 19822, Excess = 0.37},
             new PayRange {LowerLimit = 37001, Amount = 3572, Excess = 0.325},
             new PayRange {LowerLimit = 18201, Amount = 0, Excess = 0.19},
